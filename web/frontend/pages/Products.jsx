49b26f19712b7c31104e0cbcc53326de7de26267
zenmeraki7 <zenmeraki7>
2025-12-13 11:13:53 +0530

    typscript

--- web/frontend/pages/Products.jsx
+++ web/frontend/pages/Products.jsx
@@ -2,7 +2,7 @@ import { Page, Card } from "@shopify/polaris";
 import ProductTable from "../components/ProductTable/ProductTable";
 import { useProductStore } from "../state/productStore";
 
-export default function Products() {
+export default function Products(): JSX.Element {
   const products = useProductStore((s) => s.filtered);
 
   return (
